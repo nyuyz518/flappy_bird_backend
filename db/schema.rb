bfc6833c9f8a1fc37b97f7133b2b65d4004e70ad
Rose <rose1264@gmail.com>
2018-08-27 09:55:19 -0400

    Set up database for Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2018_08_21_151747) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "scores", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.integer "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -26,4 +29,5 @@ ActiveRecord::Schema.define(version: 2018_08_21_151747) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "scores", "users"
 end
